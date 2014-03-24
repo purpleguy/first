f629644d7730cf86b7756e3e726cb5402c488149
vagrant <vagrant@kellogg-dev-box>
2014-03-24 22:47:56 +0000

    Another

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Notebook::Application.routes.draw do
+
+  get "products/list"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
